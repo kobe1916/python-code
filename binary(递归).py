953bfc8d6456d1bcc2063f91432c934023315dc4
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-04-29 23:43:10 +0800

    Update binary(递归).py

--- binary(递归).py
+++ binary(递归).py
@@ -1,8 +1,8 @@
 def binary_search(arr,start,end,hkey):
-	if start > end:
+	if start > end:		#不满足条件直接返回
 		return -1
 	mid = start + (end - start) / 2
-	if arr[mid] > hkey:
+	if arr[mid] > hkey:		#递归
 		return binary_search(arr, start, mid - 1, hkey)
 	if arr[mid] < hkey:
 		return binary_search(arr, mid + 1, end, hkey)
