ae6dfdc9f4f34b274a82d6fe4ff8d326da7ac700
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-08-05 16:05:43 +0800

    Update 保存图片.py

--- 爬虫/requests/保存图片.py
+++ 爬虫/requests/保存图片.py
@@ -4,6 +4,7 @@ import requests
 r = requests.get("https://www.baidu.com/img/bd_logo1.png?where=super")
 
 #保存
+#根据文件的类型写后缀    a.png   eg: jpg  mp3/4  pdf...
 with open("a.png","wb") as f:
     f.write(r.content)
     
