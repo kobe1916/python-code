60b5aed897f3ecafdd5c6073ff04407fc037678d
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-08-05 21:52:29 +0800

    Create 贴吧爬虫.py

---
+++ 爬虫/requests/贴吧爬虫.py
@@ -0,0 +1,13 @@
+class Tiebaspider:
+    def __init__(self):
+        pass
+
+    def run(self):
+        #1.  构造url列表
+        #2.  遍历、发送请求、获取响应
+        #3.  保存
+
+
+
+if __name__ =='__main__':
+    tiebaspider = Tiebaspider("李毅"）
